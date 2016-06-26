0b42d715ac4033ec75e07486fd7a351535e412d3
Zaher <zaher88abd@gmail.com>
2016-06-26 20:00:00 -0300

    First Commit

---
+++ app/src/main/java/com/abd/zaher88/androidmoviedb/Constant.java
@@ -0,0 +1,9 @@
+package com.abd.zaher88.androidmoviedb;
+
+/**
+ * Created by Zaher on 2016-06-26.
+ */
+public class Constant {
+    private static final String MOVIE_API_KEY = String.valueOf(R.string.movie_api_key);
+
+}
---
+++ app/src/main/java/com/abd/zaher88/androidmoviedb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.abd.zaher88.androidmoviedb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

af0e94f754fdb9fe16b143620143597a215ca190
Zaher <zaher88abd@gmail.com>
2016-06-27 14:32:47 -0300

    End the main layout design

--- app/src/main/java/com/abd/zaher88/androidmoviedb/DetailsActivity.java
+++ app/src/main/java/com/abd/zaher88/androidmoviedb/DetailsActivity.java
@@ -3,11 +3,11 @@ package com.abd.zaher88.androidmoviedb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
     }
 }
